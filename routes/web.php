a7a1fa9e963fdcaf00e4f77e98c3e139a893693d
Gabriel Lozano <gabriellozanorosales@gmail.com>
2024-10-15 22:03:11 -0500

    crud,agreagar,editar,eliminar

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/cliente', [ClienteController::class, 'index']);
 Route::get('/factura', [FacturaController::class, 'index']);
 Route::get('/informes', [informesController::class, 'index']);
 Route::get('/presupuesto', [PresupuestoController::class, 'index']);
+
+//ruta agregar nuevo cliente
+Route::post("/registrar-cliente", [ClienteController::class, "create"])->name("crud.create");
+//ruta modificar cliente
+Route::post("/modificar-cliente", [ClienteController::class, "update"])->name("crud.update");
+//ruta eliminar cliente 
+Route::get("/eliminar-cliente-{id}", [ClienteController::class, "delete"])->name("crud.delete");
